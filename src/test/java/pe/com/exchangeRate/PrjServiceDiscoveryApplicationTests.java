443ff71b26da45a42a55cf0d183d25e9f507673f
unknown <vmillamora@gmail.com>
2023-11-28 00:42:13 -0500

    comitenado proj-service-discovery

---
+++ src/test/java/pe/com/exchangeRate/PrjServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.exchangeRate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
